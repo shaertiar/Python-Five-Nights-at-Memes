03c325441a55d420c5e193c029c7c5a548919bc0
Mr. Shaertiar <103391741+shaertiar@users.noreply.github.com>
2023-11-23 17:10:46 +0300

    Alpha 0.3
    Add game play process

--- game.py
+++ game.py
@@ -24,7 +24,7 @@ class Game:
         self.menu_display = MenuDisplay(self.window)
         self.settings_display = SettingDisplay(self.window, self.language)
         self.backstory = BackStory(self.window)
-        self.game_play = GamePlay(self.window)
+        self.gameplay = GamePlay(self.window)
         self.music_volume = 100
         self.effects_volume = 100
 
@@ -40,10 +40,11 @@ class Game:
             self.backstory.draw()
 
         elif self.stage == 'gameplay':
-            self.game_play.draw()
+            self.gameplay.draw()
 
         if self.backstory.is_next_stage == True:
             self.stage = 'gameplay'
+            self.backstory.is_next_stage = False
 
     # Функция обработки нажатий клавиш
     def pressed_on(self, key):
@@ -121,6 +122,9 @@ class Game:
 
                 button_num += 1
 
+        elif self.stage == 'gameplay':
+            self.gameplay.clicked_on(mouse_pos)
+
 # Класс меню
 class MenuDisplay:
     # Конструктор
@@ -144,7 +148,7 @@ class MenuDisplay:
         self.window.blit(self.title_font.render('Five Night\'s at Memes', True, (255, 0, 0)), (50, 50))
         for button in self.buttons:
             self.window.blit(button, (self.buttons[button].x, self.buttons[button].y))
-        self.window.blit(self.subtitles_font.render('Made by Shaertiar', True, (255, 255, 255)), (6, 780))
+        self.window.blit(self.subtitles_font.render('Made by Shaertiar (Alpha 0.2)', True, (255, 255, 255)), (6, 780))
         self.window.blit(pg.transform.scale(pg.image.load(r'img\Freddy Fazbear main.png'), (800, 800)), (400, 200))
 
 # Класс настроек
@@ -186,6 +190,9 @@ class SettingDisplay:
             self.buttons_font.render(self.language, True, (255, 0, 0)): pg.rect.Rect(50, 500, 60, 57)
         }
 
+        settings = json.load(open(r'settings.json', 'r'))['language'] = self.language
+        json.dump(settings, open(r'settings.json', 'w'))
+
 # Класс предистории
 class BackStory:
     # Конструктор
@@ -219,22 +226,181 @@ class BackStory:
         music = pg.mixer.Sound(r'sound\Backstory.mp3')
         pg.mixer.Channel(0).play(music)
 
-# Класс игры
+# Класс игрового процесса
 class GamePlay:
     # Конструктор
     def __init__(self, window):
         self.window = window
         self.stage = 'office'
-        self.office = ...
-        self.screen = ...
+        self.text_font = pg.font.SysFont('impact', 25)
+        self.cam_font = pg.font.SysFont('consolas', 20)
+        self.cam_small_font = pg.font.SysFont('consolas', 15)
+        self.cam_num = 1
+        self.is_cam_ventilation = False
+        self.game_display = GameDisplay(self.window)
+        self.buttons = {
+            self.text_font.render(f'{"Open":^300}', True, (255, 255, 255), (123, 0, 0)): pg.rect.Rect(0, 769, 1200, 31),
+        }
+        self.time = 7200
 
     # Функция отображения
     def draw(self):
-        if self.stage == 'office':
-            self.office.draw()
+        self.window.fill((0, 0, 0))
+        # self.game_display.draw(self.stage)
 
-        elif self.stage == 'screen':
-            self.screen.draw()
+        # Отображение времени
+        self.window.blit(self.text_font.render(f'{self.return_time()}', True, (255, 255, 255)), (0, 0))
+
+        # Если открыт экран
+        if self.stage == 'display':
+            # Если включена камера винтеляции
+            if self.is_cam_ventilation:
+                # Отображаем карту вентеляции
+                self.window.blit(pg.transform.scale(pg.image.load(r'img\ventilation map.png'), (300, 300)), (900, 469))
+                # Создаем кнопки вентеляции
+                self.buttons = {
+                    self.text_font.render(f'{"Open":^300}', True, (255, 255, 255), (123, 0, 0)): pg.rect.Rect(0, 769, 1200, 31),
+                    self.cam_font.render(' V ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1120, 685, 42, 20),
+                    self.cam_small_font.render(' 10', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1139, 735, 24, 15),
+                    self.cam_small_font.render(' 11', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(957, 702, 24, 15),
+                    self.cam_small_font.render(' 12', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1037, 740, 24, 15)
+                }
+            # Если камера вентеляции не включена
+            else:
+                # Отображаем обычную карту
+                self.window.blit(pg.transform.scale(pg.image.load(r'img\map.png'), (300, 300)), (900, 469))
+                # Создаем кнопки без вентеляции
+                self.buttons = {
+                    self.text_font.render(f'{"Open":^300}', True, (255, 255, 255), (123, 0, 0)): pg.rect.Rect(0, 769, 1200, 31),
+                    self.cam_font.render(' V ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1120, 685, 42, 20),
+                    self.cam_font.render(' 1 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1105, 484, 42, 20),
+                    self.cam_font.render(' 2 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(972, 649, 42, 20),
+                    self.cam_font.render(' 3 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1073, 627, 42, 20),
+                    self.cam_font.render(' 4 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(990, 482, 42, 20),
+                    self.cam_font.render(' 5 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(905, 628, 42, 20),
+                    self.cam_font.render(' 6 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(971, 731, 42, 20),
+                    self.cam_small_font.render(' 7 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(938, 654, 24, 15),
+                    self.cam_small_font.render(' 8 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1050, 727, 24, 15),
+                    self.cam_small_font.render(' 9 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1089, 727, 24, 15),
+                }
+
+            # Отображаем все нужные кнопки
+            for button in self.buttons:
+                self.window.blit(button, (self.buttons[button].x, self.buttons[button].y))
+
+        # Отображаем лишь кнопку открытия монитора
+        else:
+            for button in self.buttons:
+                self.window.blit(button, (self.buttons[button].x, self.buttons[button].y))
+                break
+
+        # Уменьшаем время (увеличиваем)
+        self.time -= 1
+
+    # Функция обработки нажатий мыши
+    def clicked_on(self, mouse_pos):
+        is_need_change = False
+
+        button_num = 0
+        for button in self.buttons:
+            if self.buttons[button].collidepoint(mouse_pos[0], mouse_pos[1]):
+                click_sound = pg.mixer.Sound(r'sound\Click3.mp3')
+                pg.mixer.Channel(1).play(click_sound)
+
+                # Нажатие на кнопку открытия монитора
+                if button_num == 0:
+                    # Звук нажатия
+                    click_sound = pg.mixer.Sound(r'sound\Click2.mp3')
+                    pg.mixer.Channel(1).play(click_sound)
+
+                    if self.stage == 'office': 
+                        self.stage = 'display'
+
+                    else:
+                        self.stage = 'office'
+
+                    is_need_change = True
+
+                # Нажатие на кнопку отображения вентиляционной карты
+                elif button_num == 1:
+                    self.is_cam_ventilation = not self.is_cam_ventilation
+
+            button_num += 1
+
+        if is_need_change:
+            if self.stage == 'office':
+                if self.is_cam_ventilation:
+                    self.buttons = {
+                        self.text_font.render(f'{"Open":^300}', True, (255, 255, 255), (123, 0, 0)): pg.rect.Rect(0, 769, 1200, 31),
+                        self.cam_font.render(' V ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1120, 685, 42, 20),
+                        self.cam_small_font.render(' 10', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1139, 735, 24, 15),
+                        self.cam_small_font.render(' 11', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(957, 702, 24, 15),
+                        self.cam_small_font.render(' 12', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1037, 740, 24, 15)
+                    }
+                else:
+                    self.buttons = {
+                        self.text_font.render(f'{"Open":^300}', True, (255, 255, 255), (123, 0, 0)): pg.rect.Rect(0, 769, 1200, 31),
+                        self.cam_font.render(' V ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1120, 685, 42, 20),
+                        self.cam_font.render(' 1 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1105, 484, 42, 20),
+                        self.cam_font.render(' 2 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(972, 649, 42, 20),
+                        self.cam_font.render(' 3 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1073, 627, 42, 20),
+                        self.cam_font.render(' 4 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(990, 482, 42, 20),
+                        self.cam_font.render(' 5 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(905, 628, 42, 20),
+                        self.cam_font.render(' 6 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(971, 731, 42, 20),
+                        self.cam_small_font.render(' 7 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(938, 654, 24, 15),
+                        self.cam_small_font.render(' 8 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1050, 727, 24, 15),
+                        self.cam_small_font.render(' 9 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1089, 727, 24, 15),
+                    }
+            else:
+                if self.is_cam_ventilation:
+                    self.buttons = {
+                        self.text_font.render(f'{"Close":^300}', True, (255, 255, 255), (123, 0, 0)): pg.rect.Rect(0, 769, 1200, 31),
+                        self.cam_font.render(' V ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1120, 685, 42, 20),
+                        self.cam_small_font.render(' 10', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1139, 735, 24, 15),
+                        self.cam_small_font.render(' 11', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(957, 702, 24, 15),
+                        self.cam_small_font.render(' 12', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1037, 740, 24, 15)
+                    }
+                else:
+                    self.buttons = {
+                        self.text_font.render(f'{"Close":^300}', True, (255, 255, 255), (123, 0, 0)): pg.rect.Rect(0, 769, 1200, 31),
+                        self.cam_font.render(' V ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1120, 685, 42, 20),
+                        self.cam_font.render(' 1 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1105, 484, 42, 20),
+                        self.cam_font.render(' 2 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(972, 649, 42, 20),
+                        self.cam_font.render(' 3 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1073, 627, 42, 20),
+                        self.cam_font.render(' 4 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(990, 482, 42, 20),
+                        self.cam_font.render(' 5 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(905, 628, 42, 20),
+                        self.cam_font.render(' 6 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(971, 731, 42, 20),
+                        self.cam_small_font.render(' 7 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(938, 654, 24, 15),
+                        self.cam_small_font.render(' 8 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1050, 727, 24, 15),
+                        self.cam_small_font.render(' 9 ', True, (255, 255, 255), (123, 123, 123)): pg.rect.Rect(1089, 727, 24, 15),
+                    }
+
+    # Функция преобразования времени в красивый формат
+    def return_time(self):
+        time = 7200 - self.time
+        hours = time // 720
+        minutes = (time - hours * 720) / 120
+        return f'0{int(hours)}:{int(minutes)}0AM'
+
+    # Функция запуска музыкки
+    def start_music(self):
+        # Запуск музыки
+        music = pg.mixer.Sound(r'sound\Backstory.mp3')
+        pg.mixer.Channel(0).play(music)
+
+
+# Класс офиса
+class GameDisplay:
+
+    # Конструктор
+    def __init__(self, window):
+        self.window = window
+
+    # Функция отрисовки
+    def draw(self, stage, cam_num=0):
+        if stage == 'office':
+            self.window.blit()
+
 
 if __name__ == '__main__':
     pg.init()
---
+++ test.py
@@ -0,0 +1,11 @@
+a = {
+    'a': 0,
+    'b': 1,
+    'c': 1,
+    'd': 1,
+    'e': 1,
+}
+
+for i in a:
+    if type(a[i]) == int:
+        print('I is found')
